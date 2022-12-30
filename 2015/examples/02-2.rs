1ab185535e251e55e2c806587376dc4d54ee3706
ia0 <git@ia0.eu>
2022-12-30 23:00:23 +0100

    Solve 2015-02

---
+++ 2015/examples/02-2.rs
@@ -0,0 +1,22 @@
+use std::io::{BufRead, BufReader, Read, Write};
+
+use anyhow::Result;
+
+fn solve(input: impl Read, mut output: impl Write) -> Result<()> {
+    let mut total = 0;
+    for line in BufReader::new(input).lines() {
+        let dimensions: Vec<usize> =
+            line?.split('x').map(|x| x.parse()).collect::<Result<_, _>>()?;
+        assert_eq!(dimensions.len(), 3);
+        let mut sides = [0; 3];
+        for i in 0 .. 3 {
+            sides[i] = dimensions[i] + dimensions[(i + 1) % 3];
+        }
+        total += 2 * sides.iter().min().unwrap();
+        total += dimensions.iter().product::<usize>();
+    }
+    writeln!(output, "{total}")?;
+    Ok(())
+}
+
+adventofcode::main!(solve("examples/02.txt") == "3737498\n");
