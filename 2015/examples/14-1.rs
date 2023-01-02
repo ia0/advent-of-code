5ed0ee91f176a9b50264e1766e84d2664780e6a6
ia0 <git@ia0.eu>
2023-01-02 18:14:46 +0100

    Solve 2015-14

---
+++ 2015/examples/14-1.rs
@@ -0,0 +1,47 @@
+use std::collections::HashMap;
+use std::io::{BufRead, BufReader, Read, Write};
+
+use anyhow::Result;
+use internment::Intern;
+use regex::Regex;
+
+type Name = Intern<String>;
+
+struct Reindeer {
+    speed: usize,
+    fly: usize,
+    rest: usize,
+}
+
+impl Reindeer {
+    fn dist(&self, time: usize) -> usize {
+        let total = self.fly + self.rest;
+        let full = time / total;
+        (full * self.fly + std::cmp::min(self.fly, time % total)) * self.speed
+    }
+}
+
+fn solve(input: impl Read, mut output: impl Write) -> Result<()> {
+    let regex = Regex::new(
+        "^(.*) can fly (.*) km/s for (.*) seconds, but then must rest for (.*) seconds\\.$",
+    )?;
+    let mut reindeers = HashMap::new();
+    for line in BufReader::new(input).lines() {
+        let line = line?;
+        let capture = regex.captures(&line).unwrap();
+        assert!(reindeers
+            .insert(
+                Name::from_ref(&capture[1]),
+                Reindeer {
+                    speed: capture[2].parse()?,
+                    fly: capture[3].parse()?,
+                    rest: capture[4].parse()?,
+                },
+            )
+            .is_none());
+    }
+    writeln!(output, "{}", reindeers.values().map(|r| r.dist(2503)).max().unwrap())?;
+    Ok(())
+}
+
+adventofcode::main!(solve("examples/14.txt") == "2655\n");
