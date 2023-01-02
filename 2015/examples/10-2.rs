9fe208abe4107d878d64cb4b9aeda9a546b60854
ia0 <git@ia0.eu>
2023-01-02 13:55:00 +0100

    Solve 2015-10

---
+++ 2015/examples/10-2.rs
@@ -0,0 +1,27 @@
+#![feature(slice_group_by)]
+
+use std::io::{Read, Write};
+
+use anyhow::Result;
+
+fn step(xs: &[u8]) -> Vec<u8> {
+    let mut ys = Vec::new();
+    for xs in xs.group_by(|x, y| x == y) {
+        assert!(xs.len() < 10);
+        ys.push(xs.len() as u8 + b'0');
+        ys.push(xs[0]);
+    }
+    ys
+}
+
+fn solve(mut input: impl Read, mut output: impl Write) -> Result<()> {
+    let mut xs = Vec::new();
+    input.read_to_end(&mut xs)?;
+    for _ in 0 .. 50 {
+        xs = step(&xs);
+    }
+    writeln!(output, "{}", xs.len())?;
+    Ok(())
+}
+
+adventofcode::main!(solve("examples/10.txt") == "6989950\n");
