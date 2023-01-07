b476d583fb0980b8c725b0e639f8438c0a8201d6
ia0 <git@ia0.eu>
2023-01-07 18:34:25 +0100

    Solve 2015-24

---
+++ 2015/examples/24-1.rs
@@ -0,0 +1,35 @@
+use std::io::{BufRead, BufReader, Read, Write};
+
+use anyhow::Result;
+use number_encoding::combinadics;
+
+fn solve(input: impl Read, mut output: impl Write) -> Result<()> {
+    let weights = (BufReader::new(input).lines())
+        .map(|x| Result::Ok(x?.parse()?))
+        .collect::<Result<Vec<usize>>>()?;
+    assert!(weights.len() <= 32);
+    let sum = weights.iter().sum::<usize>();
+    assert!(sum % 3 == 0);
+    let target = sum / 3;
+    for len in 1 ..= weights.len() {
+        let mut best = None;
+        let mut iter = combinadics::Iter::new(len);
+        for _ in 0 .. number_encoding::combination(weights.len(), len) {
+            let xs = iter.get().iter().map(|&i| weights[i]);
+            if xs.clone().sum::<usize>() == target {
+                let cur = xs.product::<usize>();
+                if best.map_or(true, |best| cur < best) {
+                    best = Some(cur);
+                }
+            }
+            iter.advance();
+        }
+        if let Some(best) = best {
+            writeln!(output, "{best}")?;
+            break;
+        }
+    }
+    Ok(())
+}
+
+adventofcode::main!(solve("examples/24.txt") == "10723906903\n");
