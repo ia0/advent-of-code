2f255ac82063366002530b89f37303c22993f3a7
ia0 <git@ia0.eu>
2023-01-14 20:53:40 +0100

    Solve 2016-13

---
+++ 2016/examples/13-2.rs
@@ -0,0 +1,41 @@
+use std::cmp::Reverse;
+use std::collections::{BinaryHeap, HashSet};
+use std::io::{Read, Write};
+
+use adventofcode::Coord;
+use anyhow::Result;
+
+fn open(d: usize, p: Coord) -> bool {
+    if p.x < 0 || p.y < 0 {
+        return false;
+    }
+    let x = p.x as usize;
+    let y = p.y as usize;
+    (x * x + 3 * x + 2 * x * y + y + y * y + d).count_ones() % 2 == 0
+}
+
+fn solve(mut input: impl Read, mut output: impl Write) -> Result<()> {
+    let mut offset = String::new();
+    input.read_to_string(&mut offset)?;
+    let offset = offset.parse::<usize>()?;
+    let start = Coord { x: 1, y: 1 };
+    let mut todo = BinaryHeap::new();
+    todo.push((Reverse(0), start));
+    let mut visited = HashSet::new();
+    while let Some((Reverse(steps), cur)) = todo.pop() {
+        if !visited.insert(cur) || steps == 50 {
+            continue;
+        }
+        let steps = steps + 1;
+        for diff in adventofcode::ADJACENT_PLUS {
+            let next = cur + diff;
+            if open(offset, next) {
+                todo.push((Reverse(steps), next));
+            }
+        }
+    }
+    writeln!(output, "{}", visited.len())?;
+    Ok(())
+}
+
+adventofcode::main!(solve("examples/13.txt") == "138\n");
