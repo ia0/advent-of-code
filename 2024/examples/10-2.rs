8501534c378e812604af3cebfe35d17aeb1ed5b6
Julien Cretin <cretin@google.com>
2024-12-10 09:40:42 +0100

    Solve 2024-10

---
+++ 2024/examples/10-2.rs
@@ -0,0 +1,36 @@
+use std::collections::HashMap;
+use std::io::{BufRead, BufReader, Read, Write};
+
+use adventofcode::{Coord, ADJACENT_PLUS};
+use anyhow::Result;
+
+fn solve(input: impl Read, mut output: impl Write) -> Result<()> {
+    let mut trails: [_; 10] = std::array::from_fn(|_| HashMap::<Coord, u64>::new());
+    for (line, y) in BufReader::new(input).lines().zip(0 ..) {
+        for (byte, x) in line?.bytes().zip(0 ..) {
+            let pos = Coord { x, y };
+            assert!(byte.is_ascii_digit());
+            let height = (byte - b'0') as usize;
+            trails[height].insert(pos, (height == 9) as u64);
+        }
+    }
+    for height in (0 .. 9).rev() {
+        let (head, tail) = trails.split_at_mut(height + 1);
+        let head = head.last_mut().unwrap();
+        let tail = tail.first().unwrap();
+        for (&pos, map) in head.iter_mut() {
+            for dir in ADJACENT_PLUS {
+                let Some(count) = tail.get(&(pos + dir)) else { continue };
+                *map += count;
+            }
+        }
+    }
+    let mut total = 0;
+    for map in trails[0].values() {
+        total += map;
+    }
+    writeln!(output, "{total}")?;
+    Ok(())
+}
+
+adventofcode::main!(solve("examples/10.txt") == "1372\n");
